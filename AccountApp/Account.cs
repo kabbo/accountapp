3183932d6831f4a364a8a7ca75a83952d7338aa8
kabbo <allexe123@gmail.com>
2015-05-11 16:54:49 +0600

    Version 1.0

---
+++ AccountApp/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp
+{
+    public  class Account
+    {
+        public string accountNumber;
+        public string customarName;
+        private double balance;
+
+        public string AccountNumber
+        {
+
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        public string CustomarName
+        {
+
+            get { return this.customarName; }
+            set { this.customarName = value; }
+        }
+        public double Balance
+        {
+
+            get { return this.balance; }
+            
+        }
+
+        public double GetDeposit(double amount)
+        {
+            balance=balance + amount;
+            return balance;
+        }
+
+        public bool GetWithdraw(double amount)
+        {
+            if (amount < balance)
+            {
+                balance=balance - amount;
+                return true;
+            }
+            return false;
+        }
+
+        public string GetReport()
+        {
+            return "Customar Name : " + customarName + " your account Number : " + accountNumber + " and it's balance: " +
+                   balance + " taka ";
+        }
+
+        public void CreateAccount()
+
+        {
+            balance = 0.0;
+        }
+    }
+
+}
---
+++ AccountApp/AccountUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountApp
+{
+    public partial class AccountUI : Form
+    {
+        public AccountUI()
+        {
+            InitializeComponent();
+        }
+
+        Account account =new Account();
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            account.accountNumber = accountNumberTextBox.Text;
+            account.customarName = customarNameTextBox.Text;
+
+            account.CreateAccount();
+            MessageBox.Show(@"Account is created");
+
+            
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            account.GetDeposit(Convert.ToDouble(amountTextBox.Text));
+
+            MessageBox.Show(@"Deposit successful");
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+           
+           Boolean isSuccess= account.GetWithdraw(Convert.ToDouble(amountTextBox.Text));
+
+            if (isSuccess)
+            {
+                MessageBox.Show(@"Withdrawn successful");
+            }
+            else
+            {
+                MessageBox.Show(@"Balance is insufficient");
+            }
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show(account.GetReport());
+        }
+    }
+}
